27f6b80fd3f54b99b06299ba67e9e1b591f50708
waiyan150 <46592143+waiyan150@users.noreply.github.com>
2024-06-01 12:37:55 +0700

    feat: Add fail-over on Redis failure

--- src/main/java/com/springboot/redis/springbootredis/service/CarService.java
+++ src/main/java/com/springboot/redis/springbootredis/service/CarService.java
@@ -4,6 +4,7 @@ import io.lettuce.core.RedisConnectionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.CircuitBreaker;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -20,11 +21,7 @@ public class CarService {
     Logger logger = LoggerFactory.getLogger(CarService.class);
 
     @Cacheable(cacheNames = CAR_APP, key = "#root.methodName", sync = true)
-    @Retryable(retryFor = ConnectException.class, maxAttempts = 1, recover = "getAllRecover")
-    @CircuitBreaker(retryFor = {RedisConnectionException.class},
-            maxAttempts = 2,
-            openTimeout = 2000L,
-            resetTimeout = 20000L)
+    @Retryable(retryFor = ConnectException.class, maxAttempts = 1, recover = "getAllRecover", backoff = @Backoff(delay = 500))
     public List<String> getAll() {
         logger.info("Fetching all records...");
         return List.of("Humvee", "Toyota", "BMW");
